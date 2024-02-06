c74bed56bb7fa689b34ee92d0071d2a0ad4cb411
mjBerhe <mjberhe123@hotmail.com>
2024-02-06 13:38:54 -0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div>
+        <h1 className="text-5xl font-bold">Workout Tracker</h1>
+      </div>
+    </main>
+  );
+}
